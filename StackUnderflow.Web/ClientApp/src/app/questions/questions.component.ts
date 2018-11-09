ae3e74ca8abb64e2dd14d8615d0f31eb02192945
Rob Moore <robert.moore@allata.com>
2018-11-08 20:48:50 -0700

    started work on question list and add a question

--- StackUnderflow.Web/ClientApp/src/app/questions/questions.component.ts
+++ StackUnderflow.Web/ClientApp/src/app/questions/questions.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-questions',
@@ -7,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuestionsComponent implements OnInit {
 
-  constructor() { }
+  public questions: Question[];
+  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string, private modalService: NgbModal) {
+    http.get<Question[]>(baseUrl + 'api/Questions').subscribe(result => {
+      this.questions = result;
+    }, error => console.error(error));
+  }
 
   ngOnInit() {
   }
 
+  addQuestion(content){
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      console.log('nice')
+    }, (reason) => {
+    });
+  }
+
+}
+interface Question {
+  text: string;
+  askedBy: string;
+  askedAt: DateTimeFormat;
+  responseSolutionId: number;
+  votes: number;
 }
