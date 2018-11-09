6646bcf84b5fb79d073f646dca3f06713a67feca
River Merrill <rivermerrill.me@gmail.com>
2018-11-08 20:26:51 -0700

    Added methods to controller

--- StackUnderflow.Web/Controllers/ResponsesController.cs
+++ StackUnderflow.Web/Controllers/ResponsesController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using StackUnderflow.Business;
 using StackUnderflow.Data;
 using StackUnderflow.Entities;
 
@@ -14,113 +15,46 @@ namespace StackUnderflow.Web.Controllers
     [ApiController]
     public class ResponsesController : ControllerBase
     {
-        private readonly StackUnderflowDbContext _context;
+        private readonly ResponseService _responseService;
 
-        public ResponsesController(StackUnderflowDbContext context)
+        public ResponsesController(ResponseService responseService)
         {
-            _context = context;
-        }
-
-        // GET: api/Responses
-        [HttpGet]
-        public IEnumerable<Response> GetResponses()
-        {
-            return _context.Responses;
+            _responseService = _responseService;
         }
 
         // GET: api/Responses/5
         [HttpGet("{id}")]
         public async Task<IActionResult> GetResponse([FromRoute] int id)
         {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            var response = await _context.Responses.FindAsync(id);
-
-            if (response == null)
-            {
-                return NotFound();
-            }
-
-            return Ok(response);
-        }
-
-        // PUT: api/Responses/5
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutResponse([FromRoute] int id, [FromBody] Response response)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            if (id != response.Id)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(response).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!ResponseExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
+            return Ok(_responseService.GetResponsesByQuestionId(id));
         }
 
         // POST: api/Responses
         [HttpPost]
         public async Task<IActionResult> PostResponse([FromBody] Response response)
         {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            _context.Responses.Add(response);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetResponse", new { id = response.Id }, response);
+            return Ok(_responseService.AddResponse(response));
+        }
+        
+        // Mark as inappropriate
+        [HttpPost]
+        public async Task<IActionResult> Inappropriate([FromBody] int id)
+        {
+            return Ok(_responseService.MarkResponseAsInappropriate(id));
+        }
+        
+        // Mark as solution
+        [HttpPost]
+        public async Task<IActionResult> Solution([FromBody] int id)
+        {
+            return Ok(_responseService.MarkResponseAsSolution(id));
         }
 
         // DELETE: api/Responses/5
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteResponse([FromRoute] int id)
         {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            var response = await _context.Responses.FindAsync(id);
-            if (response == null)
-            {
-                return NotFound();
-            }
-
-            _context.Responses.Remove(response);
-            await _context.SaveChangesAsync();
-
-            return Ok(response);
-        }
-
-        private bool ResponseExists(int id)
-        {
-            return _context.Responses.Any(e => e.Id == id);
+            return Ok(_responseService.DeleteResponse(id));
         }
     }
 }
