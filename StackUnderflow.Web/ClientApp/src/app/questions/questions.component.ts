10d6ab1f0f60d3bd3e5e8577d154653ae746b773
Rob Moore <robert.moore@allata.com>
2018-11-12 19:34:30 -0700

    removed unused code

--- StackUnderflow.Web/ClientApp/src/app/questions/questions.component.ts
+++ StackUnderflow.Web/ClientApp/src/app/questions/questions.component.ts
@@ -35,15 +35,5 @@ export class QuestionsComponent implements OnInit {
     }, err => console.error(err));
     console.log(content);
   }
-
-  private getDismissReason(reason: any): string {
-    if (reason === ModalDismissReasons.ESC) {
-      return 'by pressing ESC';
-    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-      return 'by clicking on a backdrop';
-    } else {
-      return `with: ${reason}`;
-    }
-  }
 }
 
