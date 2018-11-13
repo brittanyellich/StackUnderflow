e45a36bf48a555446790451072f6e585f3d4f1ce
Brittany Heller <brittany.heller@live.com>
2018-11-12 19:39:28 -0700

    Added Question stuff

--- StackUnderflow.Web/ClientApp/src/app/questions/questions.component.ts
+++ StackUnderflow.Web/ClientApp/src/app/questions/questions.component.ts
@@ -1,5 +1,4 @@
 import {Component, OnInit} from '@angular/core';
-import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 import {environment} from "../../environments/environment";
 import {HttpClient} from "@angular/common/http";
 
@@ -13,7 +12,7 @@ export class QuestionsComponent implements OnInit {
   closeResult: string;
   public questions;
 
-  constructor(public http: HttpClient, private modalService: NgbModal) {
+  constructor(public http: HttpClient) {
     this.http.get<question[]>(`${environment.apiUrl}questions`).subscribe(result => {
       console.log(result);
       this.questions = result;
@@ -24,23 +23,7 @@ export class QuestionsComponent implements OnInit {
   }
 
   addQuestion(content){
-    this.modalService.open(content, {}).result.then((result) => {
-      console.log('nice');
-      this.closeResult = 'Closed with: ${result}';
-    }, (reason) => {
-      console.error('more modals more problems');
-      this.closeResult = 'Dismissed ${this.getDismissReason(reason)}';
-    });
-  }
 
-  private getDismissReason(reason: any): string {
-    if (reason === ModalDismissReasons.ESC) {
-      return 'by pressing ESC';
-    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-      return 'by clicking on a backdrop';
-    } else {
-      return `with: ${reason}`;
-    }
   }
 }
 
